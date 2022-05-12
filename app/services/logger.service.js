c427be4de7d92f7cddcd13d9288855b04569620c
Cesar Morales <cmorales@thedataage.com>
2022-05-12 10:07:57 -0600

    Generales de la Aplicacion

---
+++ app/services/logger.service.js
@@ -0,0 +1,35 @@
+// app/services/logger.service.js
+
+require("dotenv").config();
+var pinoms = require("pino-multi-stream");
+const pretty = require("pino-pretty");
+const SonicBoom = require("sonic-boom");
+
+const prettyStream = pretty({
+  translateTime: true,
+});
+
+let streams = [];
+
+if (process.env.NODE_ENV !== "production") {
+  streams = [{ stream: prettyStream }];
+} else {
+  const logDirectory = "./logs";
+  streams = [
+    { stream: prettyStream },
+    {
+      level: "info",
+      stream: new SonicBoom({ dest: `${logDirectory}/info.log`, mkdir: true }),
+    },
+    {
+      level: "error",
+      stream: new SonicBoom({ dest: `${logDirectory}/error.log`, mkdir: true }),
+    },
+  ];
+}
+
+const pinomsStream = pinoms({
+  streams: streams,
+});
+
+module.exports = pinomsStream;
--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
+// next.config.js
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    JWT_KEY: process.env.JWT_KEY,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
