5d27408f6a4744fe8a1164ebda1b295bb59239e2
Cesar Morales <cmorales@thedataage.com>
2023-04-12 11:26:22 -0600

    Optimizacion del Head del documenteo para cargar los estilos de bootstrap

---
+++ pages/_document.js
@@ -0,0 +1,20 @@
+// pages/_document.js
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        {/* bootstrap css */}
+        <link
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
--- src/layouts/SiteLayout/HeadSEO.jsx
+++ src/layouts/SiteLayout/HeadSEO.jsx
@@ -9,11 +9,6 @@ export default function HeadSEO() {
       <title>Sitio con NextJS</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      {/* bootstrap css */}
-      <link
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
-        rel="stylesheet"
-      />
       <link rel="icon" type="image/x-icon" href="/favicon.ico" />
     </Head>
   );
